9f4087fad49b315724014b2379c9b5a55fc96340
Alex Burneikis <alexburneikis@gmail.com>
2022-10-21 18:56:50 +1100

    proper transposition

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from game import Board
 from ai_move import get_move
+from timeit import timeit
 
-board = Board()
+def main():
+    board = Board()
 
-while not board.is_game_over():
+    while not board.is_game_over():
+        print(board)
+        print("Legal moves:", board.legal_moves())
+        print("Turn:", "Yellow" if board.turn else "Red")
+        move = get_move(board, 8)
+        board.push(move)
+
     print(board)
-    print("Legal moves:", board.legal_moves())
-    print("Turn:", "Yellow" if board.turn else "Red")
-    move = get_move(board, 9)
-    board.push(move)
+    print(board.moves)
+    print(board.result())
 
-print(board)
-print(board.moves)
-print(board.result())
+if __name__ == "__main__":
+    #time it
+    print(timeit(main, number=1))
--- minimax.py
+++ minimax.py
@@ -7,7 +7,9 @@ def minimax(board, depth, alpha, beta):
     # check if the board is in the transposition table
     hash = zobrist_hash(board)
     if hash in transposition_table:
-        return transposition_table[hash]
+        # only return the score if the depth is equal (it will never be greater)
+        if transposition_table[hash][1] == depth:
+            return transposition_table[hash][0]
 
     if depth == 0 or board.is_game_over():
         return evaluate(board)
@@ -23,7 +25,7 @@ def minimax(board, depth, alpha, beta):
                 break
 
         # add to transposition table
-        transposition_table[hash] = best_score
+        transposition_table[hash] = (best_score, depth)
 
         return best_score
     else:
@@ -38,7 +40,7 @@ def minimax(board, depth, alpha, beta):
                 break
 
         # add to transposition table
-        transposition_table[hash] = best_score
+        transposition_table[hash] = (best_score, depth)
 
         return best_score
 
@@ -52,30 +54,3 @@ def zobrist_hash(board):
             if board.red[i][j] == 1:
                 hash ^= hashlist[i*7 + j + 42]
     return hash
-
-# minimax (no transposition table)
-def minimax_nt(board, depth, alpha, beta):
-    if depth == 0 or board.is_game_over():
-        return evaluate(board)
-    if board.turn:
-        best_score = float('-inf')
-        for move in board.legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1, alpha, beta)
-            board.pop()
-            best_score = max(score, best_score)
-            alpha = max(alpha, best_score)
-            if beta <= alpha:
-                break
-        return best_score
-    else:
-        best_score = float('inf')
-        for move in board.legal_moves():
-            board.push(move)
-            score = minimax(board, depth - 1, alpha, beta)
-            board.pop()
-            best_score = min(score, best_score)
-            beta = min(beta, best_score)
-            if beta <= alpha:
-                break
-        return best_score
